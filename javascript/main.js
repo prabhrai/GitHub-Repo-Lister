2086ea8cd3f0c95fb71191f801ab389ab3a604ac
prabhrai <prabhrai94@gmail.com>
2018-01-30 10:35:11 -0500

    Added Instructions

--- javascript/main.js
+++ javascript/main.js
@@ -1,48 +1,43 @@
 
- $(document).ready(function(){
+$(document).ready(function () {
 
 
- $("#btn_showusers").on('click', function () {
-    
-            $.getJSON('https://api.github.com/users/prabhrai/repos', function (data) {
-                // console.log(data);
-                var p = document.getElementById("data");
-                p.innerHTML = "";
-    
-                $.each(data, function (i, item) {
-                    // alert(item.PageName);
-                    p.innerHTML += "<div><a href='" + item.html_url  + "'>" + "Repo " + i + " - " + item.name +  " " + " </a></div>";
-    
-                });
-    
-                // p.innerText = "JHI"; a
-                // console.log(p);
+    $("#btn_showusers").on('click', function () {
+
+        $.getJSON('https://api.github.com/users/prabhrai/repos', function (data) {
+            var p = document.getElementById("data");
+            p.innerHTML = "";
+
+            $.each(data, function (i, item) {
+                p.innerHTML += "<div><a href='" + item.html_url + "'>" + "Repo " + i + " - " + item.name + " " + " </a></div>";
+
             });
 
         });
 
+    });
 
-        $("#btn_showusers_adv").on('click', function () {
-            
-            var username = document.getElementById("usr").value;
-            if (!username) return;
 
-                    $.getJSON('https://api.github.com/users/' + username + '/repos', function (data) {
-                        // console.log(data);
-                        var p = document.getElementById("data_adv");
-                        p.innerHTML = "";
-            
-                        $.each(data, function (i, item) {
-                            // alert(item.PageName);
-                            p.innerHTML += "<div><a href='" + item.html_url  + "'>" + "Repo " + i + " - " + item.name +  " " + " </a></div>";
-            
-                        });
-            
-                        // p.innerText = "JHI"; a
-                        // console.log(p);
-                    });
-        
-                });
+    $("#btn_showusers_adv").on('click', function () {
 
-    }); 
+        var username = document.getElementById("usr").value;
+        if (!username) {
+            alert("Invalid input. Please check the username and try again.");
+            return;
+        }
+
+        $.getJSON('https://api.github.com/users/' + username + '/repos', function (data) {
+            var p = document.getElementById("data_adv");
+            p.innerHTML = "";
+
+            $.each(data, function (i, item) {
+                p.innerHTML += "<div><a href='" + item.html_url + "'>" + "Repo " + i + " - " + item.name + " " + " </a></div>";
+
+            });
+
+        });
+
+    });
+
+});
 
